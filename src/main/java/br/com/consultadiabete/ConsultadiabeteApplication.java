c60e636f15e0a188d253cd27f7000e7baedc9643
mmagc <matheusgnann@gmail.com>
2025-08-22 13:23:21 -0300

    Initial Commit

---
+++ src/main/java/br/com/consultadiabete/ConsultadiabeteApplication.java
@@ -0,0 +1,13 @@
+package br.com.consultadiabete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultadiabeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultadiabeteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/consultadiabete/ConsultadiabeteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consultadiabete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultadiabeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
