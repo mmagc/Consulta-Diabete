58c61f6b98dd479e515a95a50babfd1b56759b9a
mmagc <matheusgnann@gmail.com>
2025-10-21 19:39:38 -0300

    Refactor cors

--- src/main/java/br/com/consultadiabete/Config/CorsConfig.java
+++ src/main/java/br/com/consultadiabete/Config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://lightslategray-pigeon-168265.hostingersite.com"));
+        config.setAllowedOrigins(List.of(
+                "https://lightslategray-pigeon-168265.hostingersite.com",
+                "http://localhost:*"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/br/com/consultadiabete/Security/config/SecurityConfig.java
+++ src/main/java/br/com/consultadiabete/Security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.consultadiabete.Security.config;
 import br.com.consultadiabete.Security.Filter.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,13 +19,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/login", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
